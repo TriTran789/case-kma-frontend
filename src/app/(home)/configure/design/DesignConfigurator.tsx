d7466aaa5845c248109c1c7a49eb9ff46574d79d
TriTran <thanhtritran8@gmail.com>
2024-10-01 22:35:51 +0700

    fix json

--- src/app/(home)/configure/design/DesignConfigurator.tsx
+++ src/app/(home)/configure/design/DesignConfigurator.tsx
@@ -105,7 +105,7 @@ const DesignConfigurator = () => {
     const base64 = canvas.toDataURL();
 
     typeof window !== 'undefined' && localStorage.setItem("croppedImage", base64);
-    typeof window !== 'undefined' && localStorage.setItem("configuration", JSON.stringify(configuration));
+    typeof window !== 'undefined' && localStorage.setItem("configuration", configuration.toString());
 
     router.push(`/configure/preview`);
   }
