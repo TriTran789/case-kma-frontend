ac46f12c7fa508ef065ecd5a91c63126b514a285
TriTran <thanhtritran8@gmail.com>
2024-10-16 13:48:43 +0700

    update user dashboard

--- src/app/(home)/dashboard/users/page.tsx
+++ src/app/(home)/dashboard/users/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button } from "@headlessui/react";
 import { useAdminGetUsers } from "./actions";
 
@@ -18,10 +18,22 @@ const Users = () => {
             className="flex sm:flex-row flex-col gap-4 sm:justify-between sm:border-b sm:items-center"
           >
             <div className="flex sm:flex-row flex-col gap-4">
-              <p className="w-56">{item.name}</p>
-              <p>{item.email}</p>
+              <p className={`w-56 ${item?.is_lock && "line-through"}`}>
+                {item.name}
+              </p>
+              <p className={` ${item.is_lock && "line-through"}`}>
+                {item.email}
+              </p>
             </div>
-            <Button className="bg-red-600 rounded-lg font-semibold text-white px-4 py-2 hover:bg-red-400 max-sm:w-full">Ban</Button>
+            <Button
+              className={`${
+                item.is_lock
+                  ? "bg-blue-600 hover:bg-blue-400"
+                  : "bg-red-600 hover:bg-red-400"
+              } rounded-lg font-semibold text-white px-4 py-2 max-sm:w-full`}
+            >
+              {item?.is_lock ? "Allow" : "Ban"}
+            </Button>
           </div>
         ))}
       </div>
